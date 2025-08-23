ab77ca523635adfe21f95e8d3a54c4ee043d199e
getdryv <hello@getdryv.com>
2025-08-24 00:22:08 +0200

    Create AdminApp.jsx

---
+++ src/src/admin/src/admin/AdminApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AdminApp() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Bienvenue dans l’Admin 🚀</h1>
+    </div>
+  )
+}
