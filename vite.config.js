4d20c18f35ccf3fee7695f5cdfca965459aead60
getdryv <hello@getdryv.com>
2025-08-23 18:01:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': import.meta.env.VITE_API_BASE || 'http://localhost:4242'
-      // en dev, /api ira vers ton serveur backend
+export default defineConfig(({ mode }) => {
+  // charge les variables VITE_* depuis l'env (DEV/CI/Render)
+  const env = loadEnv(mode, process.cwd(), '') // pas de prefix filter
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        // En DEV seulement. En prod (Render Static Site), cette section est ignorée.
+        '/api': env.VITE_API_BASE || 'http://localhost:4242'
+      }
     }
   }
 })
