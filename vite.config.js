a9ba52ddc37705b09e949487550585e191d2be20
getdryv <hello@getdryv.com>
2025-08-24 12:44:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import fs from 'fs'
-
-const inputs = { main: resolve(__dirname, 'index.html') }
-const admin = resolve(__dirname, 'admin.html')
-if (fs.existsSync(admin)) inputs.admin = admin
 
 export default defineConfig({
   plugins: [react()],
-  build: { rollupOptions: { input: inputs } },
   server: {
-    proxy: { '/api': 'http://localhost:4242' } // utile uniquement en DEV
+    proxy: {
+      '/api': 'https://inscription-en-ligne-getdryv.onrender.com' // en dev, /api ira vers ton serveur
+    }
   }
 })
