8cfc15004557e83327be13705ae268f9662bc0be
getdryv <hello@getdryv.com>
2025-08-24 11:50:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import fs from 'fs'
+
+const inputs = {
+  main: resolve(__dirname, 'index.html'),
+}
+
+const adminPath = resolve(__dirname, 'admin.html')
+if (fs.existsSync(adminPath)) {
+  inputs.admin = adminPath
+}
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
-      },
+      input: inputs,
     },
   },
 })
